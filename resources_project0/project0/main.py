f9055006a56cca4e62a5cbdf3711f3262ed8fe9a
Abdoelabassi <abderrezaq@outlook.com>
2023-02-11 13:12:45 +0100

    scalar funcion done

--- resources_project0/project0/main.py
+++ resources_project0/project0/main.py
@@ -67,7 +67,10 @@ def scalar_function(x, y):
     Returns the f(x,y) defined in the problem statement.
     """
     #Your code here
-    raise NotImplementedError
+    if x <= y:
+      return x*y
+    else:
+      return x/y
 
 def vector_function(x, y):
     """
