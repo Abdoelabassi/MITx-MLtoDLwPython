40b4448c4b2e22ea81e55e4fecba13645acb360e
Abdoelabassi <abderrezaq@outlook.com>
2023-04-17 21:39:31 +0000

    KMeans algo using EM algo: evaluating the Cost function

--- Projects/project3/netflix/main.py
+++ Projects/project3/netflix/main.py
@@ -7,3 +7,20 @@ import em
 X = np.loadtxt("toy_data.txt")
 
 # TODO: Your code here
+K = [1,2,3,4]
+seeds = [0,1,2,3,4]
+Costs = []
+
+for i in K:
+    for s in seeds:
+        mixture, post = common.init(X, K=i, seed=s)
+
+        common.plot(X, mixture=mixture, post=post, title=f"Cost function with K={i}, and seed={s} ")
+
+        mixture1, post, cost = kmeans.run(X, mixture=mixture, post=post)
+        Costs.append(cost)
+
+        print(f"The cost with K={i}, and seed {s}, Cost =  {cost} ")
+    
+
+print(f"Final Costs = {Costs} ")
