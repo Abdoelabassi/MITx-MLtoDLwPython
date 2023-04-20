8c3560c3c5286319fc708fff5d36b3553b519033
Abdoelabassi <abderrezaq@outlook.com>
2023-04-20 21:30:03 +0000

    tested and found best bic, and k

--- Projects/project3/netflix/test.py
+++ Projects/project3/netflix/test.py
@@ -10,15 +10,15 @@ X = np.loadtxt("toy_data.txt")
 K = [1,2,3,4]
 n, d = X.shape
 seed = [0,1,2,3,4]
-LL = []
+BIC = {}
 # TODO: Your code here
 for k in K:
     mixture, post = common.init(X,K=k,seed=0)
     mixture, post , Log_likelihood = em.run(X, mixture=mixture, post=post)
-    common.plot(X, mixture, post, title=f"EM algo with K={k}")
-    print(f"Log likelihood={Log_likelihood}, k={k}, and seed={0} ")
-    LL.append(Log_likelihood)
-
+    bic = common.bic(X, mixture, Log_likelihood)
+    print(f"bic score={bic} for K={k} ")
+    BIC[k] = bic
+    
 print("\n")
 
-
+print(f"BIC scores = {BIC}")
